d78424e81affad6713a81835a54403a2410700f7
James Quiambao <james.quiambao@yahoo.ca>
2016-11-24 21:39:53 +0000

    Pretty much finished request header parser app

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express"); 
+var app = express(); 
+var os = require('os');
+
+
+app.get('/', function(req, res){
+    
+    var object = {
+        "ipaddress": req.headers['x-forwarded-for'], 
+        "language": req.headers["accept-language"], 
+        "software": os.platform() + " " + os.release() //'10.8.0'
+    }; 
+    
+    res.end(JSON.stringify(object));
+ 
+}); 
+
+app.listen(8080); 
