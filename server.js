9a980be93dc3b4ada8d22a0c22ce1ab173083c59
James Quiambao <james.quiambao@yahoo.ca>
2016-11-29 21:12:40 +0000

    Commented code

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+//Grab modules 
 var express = require("express"); 
 var app = express(); 
-var os = require('os');
 
+//Set port 
+var port = process.env.PORT || 8080; 
 
+//Route
 app.get('/', function(req, res){
     
+    //Return ip, language and software details 
     var object = {
         "ipaddress": req.headers['x-forwarded-for'], 
-        "language": req.headers["accept-language"], 
-        "software": os.platform() + " " + os.release() //'10.8.0'
+        "language": req.headers["accept-language"].split(",")[0] , 
+        "software": req.headers["user-agent"].match(/\((.*?)\)/)[1]
     }; 
     
+    //send json object to client 
     res.end(JSON.stringify(object));
  
 }); 
 
-app.listen(process.env.PORT || 8080); 
+//Listen 
+app.listen(port); 
